9829804b841cf5c532e364610fea2613136146da
unknown <matthewharre@gmail.com>
2021-12-26 04:33:48 -0800

    added some basic react learning examples

---
+++ react-basics/my-app/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+// this is normal react syntax
+// return html elements that we want to add
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 30th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$125.33</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
