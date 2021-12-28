eea8e784154339c3e04c81c37d897f22dea3dcf7
unknown <matthewharre@gmail.com>
2021-12-28 07:19:12 -0800

    updated src files and changed components around

--- react-basics/my-app/src/components/ExpenseItem.js
+++ react-basics/my-app/src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
-// this is normal react syntax
-// return html elements that we want to add
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    return (
-        <div className='expense-item'>
-            <ExpenseDate date={props.date}></ExpenseDate>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
