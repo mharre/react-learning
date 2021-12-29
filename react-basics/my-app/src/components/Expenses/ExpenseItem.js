1483190de2310647a63286e79fc63abe2ac12866
unknown <matthewharre@gmail.com>
2021-12-29 05:37:07 -0800

    added more organizational structure by breaking up components and also changed original functions to arrow funcs

--- react-basics/my-app/src/components/Expenses/ExpenseItem.js
+++ react-basics/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,13 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 // default HTML elements support className = ... 
 // custom HTML elements DO NOT, only support what we tell them to support
 // must change code in the actual component to allow this behavior
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
--- react-basics/my-app/src/components/UI/Card.js
+++ react-basics/my-app/src/components/UI/Card.js
@@ -1,7 +1,9 @@
+import React from 'react';
+
 import './Card.css';
 // we don't set or change children, this allows wrapper Card to work
 // anything inside of the card tags are the children / avail to use
-function Card(props) {
+const Card = (props) => {
     const classes = 'card ' + props.className;
 
     return <div className={classes}>{props.children}</div>
