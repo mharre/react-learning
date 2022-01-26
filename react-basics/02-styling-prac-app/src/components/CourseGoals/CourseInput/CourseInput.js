7b995a0631b8d39167c830119b2d90b5d2d8ecd9
unknown <matthewharre@gmail.com>
2022-01-26 09:50:33 -0800

    added some dynamic styles examples

--- react-basics/02-styling-prac-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-basics/02-styling-prac-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => props.invalid ? 'red' : 'black'};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => props.invalid ? 'red' : '#ccc'};
+    background-color: ${(props) => props.invalid ? '#ffd7d7' : 'transparent'};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
+///////////////////////////////////////////////////////////////////
+// you can pass props into your element to use them inside of `` //
+// ${(props) => props.invalid ? 'red' : '#ccc'} means            //
+// arrow func that recieves the props as parameter               //
+// should return text that should be returned in that position   //
+// if invalid is true then we return red other wise return #ccc  //
+///////////////////////////////////////////////////////////////////
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -23,12 +58,16 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
+  //<FormControl className={!isValid ? 'invalid':''}>
+  // is the same thing as below, JS trick by using &&
+  // it will render either the invalid or the other one
+  // <FormControl className={!isValid && 'invalid'}>
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
