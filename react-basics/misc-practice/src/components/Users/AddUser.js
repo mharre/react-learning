53d07632cb3a2ec6edc95e8408c42811e0f87f40
unknown <matthewharre@gmail.com>
2022-01-27 13:52:00 -0800

    added basic components and some styling

---
+++ react-basics/misc-practice/src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './AddUser.module.css';
+import Card from '../UI/Card';
+
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+            <label htmlFor='username'>Username</label>
+            <input id='username' type='text' />
+            <label htmlFor='age'>Age (Years)</label>
+            <input id='age' type='number' />
+            <button type='submit'>Add User</button>
+        </form>
+        </Card>
+    )
+};
+
+export default AddUser;
