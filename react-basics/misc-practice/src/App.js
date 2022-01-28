ed75956882aebc75f367bf142db70dea5dc3dfd2
unknown <matthewharre@gmail.com>
2022-01-28 12:59:35 -0800

    added ability to add users to the list

--- react-basics/misc-practice/src/App.js
+++ react-basics/misc-practice/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList,
+        {name: uName, age: uAge, id: Math.random().toString()},
+      ];
+    });
+    // when updating new state with setUsersList() we need special function form
+    // this is because it relies on the previous state 
+    // it relies on previous state because previous state is an empty array or in case there was anything in there previously
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
+  // we are passing the userlist component the userslist
 }
 
 export default App;
--- react-basics/misc-practice/src/components/Users/AddUser.js
+++ react-basics/misc-practice/src/components/Users/AddUser.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+        // the + before enteredAge converts it to int
+
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
+
+    const usernameChangeHandler = (event) => {
+        // this is called to keep track of the keystrokes
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        // this is called to keep track of the keystrokes
+        setEnteredAge(event.target.value);
+    };
 
     return (
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' />
+            <input id='username' type='text'
+            value={enteredUsername} onChange={usernameChangeHandler} />
             <label htmlFor='age'>Age (Years)</label>
-            <input id='age' type='number' />
+            <input id='age' type='number'
+            value={enteredAge} onChange={ageChangeHandler}/>
             <Button type='submit'>Add User</Button>
         </form>
         </Card>
