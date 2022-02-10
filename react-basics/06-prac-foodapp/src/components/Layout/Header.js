bfa871e719691f17704ebe749670955bb2cee8e5
unknown <matthewharre@gmail.com>
2022-02-10 15:59:51 -0800

    added components: CartIcon, Header, HeaderCard, AvailableMeal, Meals, MealsSummary and their respective css files

---
+++ react-basics/06-prac-foodapp/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCardButton from './HeaderCardButton';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+    return (
+       <React.Fragment>
+           <header className={classes.header}>
+               <h1>ReactMeals</h1>
+               <HeaderCardButton />
+           </header>
+           <div className={classes['main-image']}>
+               <img src={mealsImage} alt='Table of delicious food!' />
+           </div>
+       </React.Fragment>
+    )
+};
+
+export default Header;
