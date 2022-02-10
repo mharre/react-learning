baa08a18c33d68d1cd43b06d3edc47ba5ce60c9d
unknown <matthewharre@gmail.com>
2022-02-10 11:24:44 -0800

    added example use case of useImperativehandle

--- react-basics/05-effects-reducers-context/src/components/Login/Login.js
+++ react-basics/05-effects-reducers-context/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -64,6 +64,9 @@ const Login = (props) => {
 
     const context = useContext(AuthContext);
 
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
     // this is an example of object restructuring
     // taking value of isValid and alias assigning it to email/passwordIsValid
     const {isValid: emailIsValid} = emailState;      
@@ -123,13 +126,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    context.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      context.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <Input id='email'
+        <Input
+          ref={emailInputRef}
+          id='email'
           label='E-Mail'
           type='email'
           isValid={emailIsValid}
@@ -137,7 +148,9 @@ const Login = (props) => {
           onChange={emailChangeHandler}
           onBlur={validateEmailHandler}
           />
-        <Input id='email'
+        <Input
+          ref={passwordInputRef}
+          id='email'
           label='Password'
           type='password'
           isValid={passwordIsValid}
@@ -146,7 +159,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
           />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
