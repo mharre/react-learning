351e8beae928731a5df1ccbdb6425b6e54681bd8
unknown <matthewharre@gmail.com>
2022-02-12 08:18:46 -0800

    added more logic to cart, header, modal and app

--- react-basics/06-prac-foodapp/src/components/Cart/Cart.js
+++ react-basics/06-prac-foodapp/src/components/Cart/Cart.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
-    const cardItems =
-        <ul className={classes['card-item']}>{
-            [{id: 'c1', name: 'sushi', amount: 2, price: 12.99}].map((item) =>
+    const cartItems =
+        <ul className={classes['cart-items']}>{
+            [{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) =>
                 <li>{item.name}</li>)}
         </ul>
 
     return (
-        <div>
-            {cardItems}
+        <Modal onClose={props.onClose}>
+            {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>36.53</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 };
 
