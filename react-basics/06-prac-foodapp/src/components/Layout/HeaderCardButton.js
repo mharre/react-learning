584c633a275da5aff96a6daa3dbadfff5c7ea6a7
unknown <matthewharre@gmail.com>
2022-02-13 10:31:24 -0800

    added logic for adding items to cart with useContext

--- react-basics/06-prac-foodapp/src/components/Layout/HeaderCardButton.js
+++ react-basics/06-prac-foodapp/src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCardButton.module.css';
 
 const HeaderCardButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+    //reduce allows us to turn array of data into a single val
+    //curNumber, item are 2 things always return by js
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>
-                3
-            </span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 };
