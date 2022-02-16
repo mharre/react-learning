3cea69bf233e78dff5c48bf1119ba2af4649fab5
unknown <matthewharre@gmail.com>
2022-02-16 15:59:45 -0800

    added examples that show how react works behind the scenes

--- react-basics/misc-behindthescenes/src/App.js
+++ react-basics/misc-behindthescenes/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback }from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+      //this doesn't work because callback saved allowToggle initial value and it does not change no matter what due to our use of callback
+      //so when it runs again it just uses the variable information that was saved initially only, and therefore doesn;t work
+      // we should allowToggle as a dependency to get around this issue in the aray
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
