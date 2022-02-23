195440387914b7c1fdfbbff4a75e6c6019c197aa
unknown <matthewharre@gmail.com>
2022-02-23 11:05:15 -0800

    added some error handling and cleaned up the fetching function

--- react-basics/06-prac-foodapp/src/components/Meals/AvailableMeals.js
+++ react-basics/06-prac-foodapp/src/components/Meals/AvailableMeals.js
@@ -6,13 +6,17 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = (props) => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
    useEffect(() => { 
-    async function fetchMovies() {
+    const fetchMeals = async () => {
+      setError(null);
       const response = await fetch('https://react-http-10a52-default-rtdb.firebaseio.com/meals.json');
   
       if (!response.ok) {
         throw new Error('Something went wrong!');
+        // when we generate error we get an obj, by default has a .message property
       }
   
       const data = await response.json();
@@ -27,9 +31,29 @@ const AvailableMeals = (props) => {
         })
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     }
-    fetchMovies();
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+    // we have to catch the error like this instead of wrapping in try {} and catch (error) {} because
+    // fetchMeals() is an async func that returns promise, if throw error inside promise that error will cause that promise to reject
+    // we can't use try catch to wrap because we'd need to turn the fetchMeals with await and turn the first func in useEffect with async (not allowed with react)
   }, []);
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p> Loading... </p>
+    </section>
+  }
+
+  if (error) {
+    return <section className={classes.MealsError}>
+      <p>{error}</p>
+    </section>
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
