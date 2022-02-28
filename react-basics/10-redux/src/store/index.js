cf5dcac5dc2a95e78c0a3c3b3155f3e57e4bda00
unknown <matthewharre@gmail.com>
2022-02-28 14:08:21 -0800

    added reduxjs tools example, cleaned up code to encorporate reduxjs toolkit

--- react-basics/10-redux/src/store/index.js
+++ react-basics/10-redux/src/store/index.js
@@ -1,42 +1,37 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
     counter: 0,
     showCounter: true
 };
 
-const counterReducer = (state=initialState, action) => {
-    if(action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state,action) {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
         }
     }
+});
 
-    if(action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+// only 1 .reducer can be passed to createStore so multile slices could present issue. 
+// configureStore solves this, must pass configuration object where we set a reducer property
+// reducer can only be used once in configureStore but we can set it as an object with a key of our choice and the value would be differnt reducer functions
 
-    if(action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
-
+export const counterActions = counterSlice.actions
+// creatslice automaticallyyy creates unique action identifiers for our reducers
+// methods of .actions are the names that we have in our reducers object in the slice (inc, dec.. etc) all done automatically by toolkit
 export default store;
