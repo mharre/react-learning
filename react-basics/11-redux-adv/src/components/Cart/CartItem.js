61c036d0d50b425da973bfc144f8f2d2ec2fa56d
unknown <matthewharre@gmail.com>
2022-03-01 13:48:10 -0800

    added a lot of different components, abilities to add and remove items all with dispatching actions, logic for cart added

--- react-basics/11-redux-adv/src/components/Cart/CartItem.js
+++ react-basics/11-redux-adv/src/components/Cart/CartItem.js
@@ -1,7 +1,23 @@
 import classes from './CartItem.module.css';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price,  id } = props.item;
+
+  const removeItemHander = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHander = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHander}>-</button>
+          <button onClick={addItemHander}>+</button>
         </div>
       </div>
     </li>
---
+++ react-basics/11-redux-adv/src/store/cart-slice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCartState = {
+    items: [],
+    totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: initialCartState,
+    reducers: {
+        addItemToCart(state,action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find(item => item.id === newItem.id)
+            state.totalQuantity++;
+            if (!existingItem) { // OKAY with redux toolkit, not with just redux
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                }); 
+            } else {
+                existingItem.quantity++
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state,action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id != id);
+                // keep all items where the id does not match the id we are trying to remove
+                // creates new array 
+            } else {
+                existingItem.quantity--
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price; 
+            }
+        }
+    }
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
