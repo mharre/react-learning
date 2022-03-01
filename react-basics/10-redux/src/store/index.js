1718508681c92b96774254b49c391ec13a05fae5
unknown <matthewharre@gmail.com>
2022-02-28 16:06:18 -0800

    added more organization to mimic larger applications, moved slices into own folders

--- react-basics/10-redux/src/store/index.js
+++ react-basics/10-redux/src/store/index.js
@@ -1,55 +1,10 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = {
-    counter: 0,
-    showCounter: true
-};
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state,action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
-});
-
-const intialAuthState = {
-    isAuthenticated: false
-};
-
-const authSlice = createSlice({
-    name: 'authentication',
-    initialState: intialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        },
-    },
-});
+import counterReducer from './counter';
+import authReducer from './auth';
 
 const store = configureStore({
-    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+    reducer: {counter: counterReducer, auth: authReducer}
 });
-// only 1 .reducer can be passed to createStore so multile slices could present issue. 
-// configureStore solves this, must pass configuration object where we set a reducer property
-// reducer can only be used once in configureStore but we can set it as an object with a key of our choice and the value would be differnt reducer functions
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
-// creatslice automaticallyyy creates unique action identifiers for our reducers
-// methods of .actions are the names that we have in our reducers object in the slice (inc, dec.. etc) all done automatically by toolkit
 export default store;
