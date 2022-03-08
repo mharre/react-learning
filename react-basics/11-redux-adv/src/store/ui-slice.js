fce193fa28539bdc2a1278ddcb1e65c90c464344
unknown <matthewharre@gmail.com>
2022-03-08 12:13:53 -0800

    added ability to add cart items to backend, error handling, and the example with useEffect to control these actions

--- react-basics/11-redux-adv/src/store/ui-slice.js
+++ react-basics/11-redux-adv/src/store/ui-slice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialUIState = {
-    cartIsVisible: false
+    cartIsVisible: false,
+    notification: null
 };
 
 const uiSlice = createSlice({
@@ -10,6 +11,13 @@ const uiSlice = createSlice({
     reducers: {
         toggle(state) {
             state.cartIsVisible = !state.cartIsVisible
+        },
+        showNotification(state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            };
         },
     }
 });
