e9e2d6c3ac9bc3670af6ef30b0468dec854406b4
unknown <matthewharre@gmail.com>
2022-03-08 13:59:30 -0800

    added the same logic but with a thunk and action creator instead, cleaned up the code in app.js and transferred to cart-slice.js

--- react-basics/11-redux-adv/src/App.js
+++ react-basics/11-redux-adv/src/App.js
@@ -4,8 +4,8 @@ import { useEffect, Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -16,45 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }));
-
-      const response = await fetch('https://react-http-10a52-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT', // PUT will override existing data instead of just POST
-        body: JSON.stringify(cart),
-      })
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed');
-      }
-
-      //const responseData = await response.json(); // if we make it past this line we know it was sucessful yet we do not even need response data for this specific example, error is enough
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!'
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
-    } //blocks data to be sent when it first loads, if items in cart since it is a put request on the first initial load it resends a request and overrides the data with 0 items
+    }
 
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data has failed'
-      }));
-    });
+    dispatch(sendCartData(cart));
 
   }, [cart, dispatch]);
 
--- react-basics/11-redux-adv/src/store/cart-slice.js
+++ react-basics/11-redux-adv/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const initialCartState = {
     items: [],
@@ -41,6 +42,47 @@ const cartSlice = createSlice({
         }
     }
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {  
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const sendRequest = async () => {
+        const response = await fetch('https://react-http-10a52-default-rtdb.firebaseio.com/cart.json', {
+            method: 'PUT', // PUT will override existing data instead of just POST
+            body: JSON.stringify(cart),
+        });
+    
+        if (!response.ok) {
+            throw new Error('Sending cart data failed');
+        }
+      };
+      
+      try {
+        await sendRequest();
+        dispatch(
+            uiActions.showNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'Sent cart data successfully!'
+            })
+        );
+      } catch (error) {
+        dispatch(uiActions.showNotification({
+            status: 'error',
+            title: 'Error!',
+            message: 'Sending cart data has failed'
+            })
+        );
+      }
+    };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
